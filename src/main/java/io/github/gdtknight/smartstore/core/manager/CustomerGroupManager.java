857ca09259f5808dd4f910e270f242990a722da1
YongHo Shin <gdtknight@naver.com>
2023-06-07 13:07:05 +0900

    feat: #2 - add CustomerGroupManager class

---
+++ src/main/java/io/github/gdtknight/smartstore/core/manager/CustomerGroupManager.java
@@ -0,0 +1,60 @@
+package io.github.gdtknight.smartstore.core.manager;
+
+import io.github.gdtknight.smartstore.core.domain.CustomerGroup;
+import io.github.gdtknight.smartstore.enums.CustomerType;
+import java.util.Arrays;
+
+/**
+ * 스마트스토어의 모든 고객 그룹, 세분화 기준 저장 (Database 대체용)
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
+public class CustomerGroupManager {
+  private static CustomerGroupManager customerGroupManager = new CustomerGroupManager();
+  private final CustomerGroup[] customerGroups;
+
+  private CustomerGroupManager() {
+    customerGroups =
+        Arrays.stream(CustomerType.values())
+            .map(customerType -> new CustomerGroup(customerType, null))
+            .toArray(CustomerGroup[]::new);
+  }
+
+  public static CustomerGroupManager getInstance() {
+    if (customerGroupManager == null) {
+      customerGroupManager = new CustomerGroupManager();
+    }
+    return customerGroupManager;
+  }
+
+  /**
+   * 고객 그룹 저장
+   *
+   * @param customerGroup 고객 그룹
+   */
+  public void save(CustomerGroup customerGroup) {
+    Arrays.stream(customerGroups)
+        .filter(group -> group.getCustomerType() == customerGroup.getCustomerType())
+        .forEach(group -> group.setParameter(customerGroup.getParameter()));
+  }
+
+  /**
+   * @param customerType 고객 유형
+   * @return 해당 고객 그룹
+   */
+  public CustomerGroup selectCustomerGroupByCustomerType(CustomerType customerType) {
+    return Arrays.stream(customerGroups)
+        .filter(customerGroup -> customerGroup.getCustomerType() == customerType)
+        .findFirst()
+        .orElse(null);
+  }
+
+  /**
+   * @return 모든 고객 그룹
+   */
+  public CustomerGroup[] selectAllCustomerGroup() {
+    return Arrays.stream(customerGroups).toArray(CustomerGroup[]::new);
+  }
+}
