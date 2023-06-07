906c96f01521d44c165b30041b3e54799ba5f2c8
YongHo Shin <gdtknight@naver.com>
2023-06-07 13:07:15 +0900

    feat: #2 - add CustomerGroupService class

---
+++ src/main/java/io/github/gdtknight/smartstore/core/service/CustomerGroupService.java
@@ -0,0 +1,74 @@
+package io.github.gdtknight.smartstore.core.service;
+
+import static io.github.gdtknight.smartstore.exceptions.AppErrorCode.NO_MATCHING_GROUP;
+
+import io.github.gdtknight.smartstore.core.domain.CustomerGroup;
+import io.github.gdtknight.smartstore.core.domain.CustomerGroupDto;
+import io.github.gdtknight.smartstore.core.domain.Parameter;
+import io.github.gdtknight.smartstore.core.manager.CustomerGroupManager;
+import io.github.gdtknight.smartstore.enums.CustomerType;
+import io.github.gdtknight.smartstore.exceptions.AppException;
+import java.util.Arrays;
+
+/**
+ * 고객 유형 관리 서비스 제공 클래스
+ *
+ * @author YongHo Shin
+ * @version v1.0
+ * @since 2023-05-10
+ */
+public class CustomerGroupService {
+  private static CustomerGroupService customerGroupService = new CustomerGroupService();
+  private final CustomerGroupManager customerGroupManager = CustomerGroupManager.getInstance();
+  private final CustomerService customerService = CustomerService.getInstance();
+
+  public static CustomerGroupService getInstance() {
+    if (customerGroupService == null) {
+      customerGroupService = new CustomerGroupService();
+    }
+    return customerGroupService;
+  }
+
+  /**
+   * 그룹 파라미터 설정 후 저장
+   *
+   * @param customerGroupDto 고객 그룹 DTO
+   * @param parameter 분류 기준
+   * @return 설정이 완료된 고객 그룹
+   */
+  public CustomerGroupDto updateGroupParameter(
+      CustomerGroupDto customerGroupDto, Parameter parameter) {
+
+    customerGroupDto.parameter().update(parameter);
+
+    customerGroupManager.save(CustomerGroupDto.toEntity(customerGroupDto));
+
+    customerService.classifyAllCustomers();
+
+    return customerGroupDto;
+  }
+
+  /**
+   * @param customerType 고객 유형
+   * @return 고객 유형과 일치하는 그룹
+   * @throws AppException 데이터베이스에 일치하는 그룹이 없을 경우
+   */
+  public CustomerGroupDto find(CustomerType customerType) throws AppException {
+    CustomerGroup customerGroup =
+        customerGroupManager.selectCustomerGroupByCustomerType(customerType);
+
+    if (customerGroup == null) throw new AppException(NO_MATCHING_GROUP);
+
+    return CustomerGroupDto.fromEntity(customerGroup);
+  }
+
+  /**
+   * @return 데이터베이스에 저장된 모든 고객 그룹
+   * @throws AppException 데이터베이스 오류
+   */
+  public CustomerGroupDto[] findAll() throws AppException {
+    return Arrays.stream(customerGroupManager.selectAllCustomerGroup())
+        .map(CustomerGroupDto::fromEntity)
+        .toArray(CustomerGroupDto[]::new);
+  }
+}
